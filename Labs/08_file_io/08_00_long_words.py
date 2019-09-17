cb0b318aa309a84ab52936f57b00d49072cd648c
Darja <puzanovad@email.cz>
2019-09-17 13:49:24 +0200

    new labs finished - Classes - file io

--- Labs/08_file_io/08_00_long_words.py
+++ Labs/08_file_io/08_00_long_words.py
@@ -2,3 +2,15 @@
 Write a program that reads words.txt and prints only the words
 with more than 20 characters (not counting whitespace).
 '''
+
+with open("words.txt","r") as fin:
+    list1 = []
+
+
+    for w in fin.readlines():
+        w = w.rstrip()
+        if len(w) > 20:
+            list1.append(w)
+
+
+    print(list1)
